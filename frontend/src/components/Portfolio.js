238b21a0dfd5abd473d562a598904f4057bd10e8
cakuang1 <70300980+cakuang1@users.noreply.github.com>
2023-08-12 23:25:55 -0700

    side bar issues

--- frontend/src/components/Portfolio.js
+++ frontend/src/components/Portfolio.js
@@ -7,9 +7,10 @@ let exampledata = {
   id : 'f47ac10b-58cc-4372-a567-0e02b2c3d479' ,
   ticker: 'AAPL',
   name : 'Apple Inc',
+  purchasedate : '',
   boughtfor: '127.87',     
   ammountbought : '20',
-  total : '',
+  total : '1500',
 }
 
 
@@ -21,11 +22,11 @@ const StockCards = ({data}) => {
     <h1 class="text-xl font-semibold mb-2">Purchase ID: {data.id}</h1>
     <h2 class="text-lg text-gray-700 mb-1">{data.ticker}</h2>
     <h3 class="text-md text-gray-500 mb-3">{data.name}</h3>
-    <div class="flex items-center space-x-4 mt-3 border h-20">
-    <div class="text-lg font-medium text-blue-700">${data.boughtfor}</div>
-    <div class="text-md text-gray-600">Quantity: <span class="text-gray-800">{data.ammountbought}</span></div>
-    <div class="text-lg font-medium text-green-700">${data.total}</div>
-</div>
+    <div class="flex items-center space-x-4 mt-3 border h-20 bg-green-50">
+      <div class="text-lg font-medium text-blue-700">${data.boughtfor}</div>
+      <div class="text-md text-gray-600">Quantity: <span class="text-gray-800">{data.ammountbought}</span></div>
+      <div class="text-lg font-medium text-green-700">${data.total}</div>
+  </div>
 
 </div>
     )
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,36 +1,49 @@
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink,useLocation} from 'react-router-dom';
 import { useState } from 'react';
 
 
-function SidebarButton({ to, icon, text }) {
-  return (
-    <NavLink to={to} className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-green-200 pr-6">
-                <a class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
 
-                {portfolio}
-                <span class="mx-2 text-sm font-medium">Portfolio</span>
-                </a>
-    </NavLink>
+
+function SidebarButton({ to, icon, text ,active}) {
+
+  return (
+<NavLink
+  to={to}
+  className={`relative flex flex-row items-center h-11 focus:outline-none text-gray-600  border-l-4 border-transparent hover:border-green-200 pr-6 ${
+    active  ? 'bg-green-100' : ''
+  }`}>
+  <a className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg">
+    {icon}
+    <span className="mx-2 text-xl font-medium">{text}</span>
+  </a>
+</NavLink>
+
+
   );
 };
 
 
-let portfolio = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+let portfolio = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
 </svg>
 
 
-let stockimage = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+let stockimage = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
 <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
 </svg>
 
+let abuoutimage = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+</svg>
 
 function Sidebar(){
-
-  const currentRoute = '/current/route'
-
+  const location = useLocation();
+  const isPortfolioActive = location.pathname === '/portfolio';
+  const isStocksActive = location.pathname === '/stocks';
+  const isAboutActive = location.pathname === '/about';
+  
   return (
 <aside class="flex flex-col w-80 h-screen px-5 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
 
@@ -39,27 +52,13 @@ function Sidebar(){
         <nav class="-mx-3 space-y-6 ">
             <div class="space-y-3 ">
                 <label class="px-3 text-s text-gray-500  dark:text-gray-400">Main</label>
-                <SidebarButton to={'/portfolio'} icon={portfolio} text={'Portfolio'} />
-
-
-                <a class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                    {stockimage}
-
-                    <span class="mx-2 text-xl font-medium">Stocks</span>
-                </a>
+                <SidebarButton to={'/portfolio'} icon={portfolio} text={'Portfolio'} active={isPortfolioActive}/>
+                <SidebarButton to = {'/stocks'} icon = {stockimage} text = {"Stocks"} active={isStocksActive}/>
             </div>
-
             <div class="space-y-3 ">
                 <label class="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">Misc</label>
 
-                <a class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                    </svg>
-
-                    <span class="mx-2 text-sm font-medium">About</span>
-                </a>
-
+                <SidebarButton to = {"/about"} icon = {abuoutimage} text={'About'} />
 
             </div>
 
