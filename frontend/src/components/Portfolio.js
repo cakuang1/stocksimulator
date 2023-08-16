425496b3f2e83bb7d672ed759572ee2380912469
cakuang1 <70300980+cakuang1@users.noreply.github.com>
2023-08-16 14:43:48 -0700

    portfolio

--- frontend/src/components/Portfolio.js
+++ frontend/src/components/Portfolio.js
@@ -15,6 +15,12 @@ let exampledata = {
 
 
 
+function getPurchases() {
+  return JSON.parse(localStorage.getItem('purchases')) || [];
+}
+
+
+
 
 const StockCards = ({data}) => {
     return (        
@@ -22,17 +28,42 @@ const StockCards = ({data}) => {
     <h1 class="text-xl font-semibold mb-2">Purchase ID: {data.id}</h1>
     <h2 class="text-lg text-gray-700 mb-1">{data.ticker}</h2>
     <h3 class="text-md text-gray-500 mb-3">{data.name}</h3>
-    <div class="flex items-center space-x-4 mt-3 border h-20 bg-green-50">
-      <div class="text-lg font-medium text-blue-700">${data.boughtfor}</div>
-      <div class="text-md text-gray-600">Quantity: <span class="text-gray-800">{data.ammountbought}</span></div>
-      <div class="text-lg font-medium text-green-700">${data.total}</div>
+    <h3 class="text-md text-gray-500 mb-3">{data.purchasedate}</h3>
+    <div class="flex items-center space-x-4 mt-3 gap-4 h-32 justify-center">
+    <div className='text-center'>
+      <p class="mb-2 text-xl font-medium text-gray-600 dark:text-gray-400">
+        Purchase Price
+      </p>
+      <p class="text-l font-bold text-gray-700 dark:text-gray-200">
+        ${data.boughtfor}
+      </p>
+    </div>
+    <div className='text-center'> 
+      <p class="mb-2 text-xl font-medium text-gray-600 dark:text-gray-400">
+        Amount
+      </p>
+      <p class="text-l font-bold text-gray-700 dark:text-gray-200">
+        {data.ammountbought}
+      </p>
+    </div>
+    <div className='text-center'>
+      <p class="mb-2 text-xl font-medium text-gray-600 dark:text-gray-400">
+        Total Spent
+      </p>
+      <p class="text-l font-bold text-gray-700 dark:text-gray-200">
+        ${data.total}
+      </p>
+    </div>
   </div>
-
 </div>
     )
 }
 
 function Portfolio(){
+
+
+
+  
     localStorage.setItem('test','test')
     return (
 <div className='innerlayer bg-white h-[calc(100vh-2rem)] w-11/12 m-auto border rounded-2xl flex flex-col justify-between'>
@@ -41,11 +72,53 @@ function Portfolio(){
 </div>
 
 
-<div className=" text-white p-4 mx-10 gap-4 justify-center h-40 mb-8 flex">
-      <div className='bg-green-50 w-80'>test</div>
-      <div className='bg-green-50 w-80'></div>
-      <div className='bg-green-50 w-80'></div>
+<div class="flex justify-center mb-10">
+  <div className="h-32 w-96 rounded-lg shadow-xs border ">
+    <div className="flex  items-center p-4">
+      <div className="p-3 rounded-full  dark:text-orange-100 bg-green-100 dark:bg-orange-500 mr-4">
 
+      </div>
+    <div>
+      <p class="mb-2 text-3xl font-medium text-gray-600 dark:text-gray-400">
+        PORTFOLIO
+      </p>
+      <p class="text-5xl font-bold text-gray-700 dark:text-gray-200">
+
+      </p>
+    </div>
+    </div>
+  </div>
+  <div className="h-32 w-96 rounded-lg shadow-xs border ">
+    <div className="flex  items-center p-4">
+      <div className="p-3 rounded-full  dark:text-orange-100 bg-green-100 dark:bg-orange-500 mr-4">
+
+
+      </div>
+    <div>
+      <p class="mb-2 text-3xl font-medium text-gray-600 dark:text-gray-400">
+        PORTFOLIO CHANGE
+      </p>
+      <p class="text-5xl font-bold text-gray-700 dark:text-gray-200">
+        holder
+      </p>
+    </div>
+    </div>
+  </div>
+  <div className="h-32 w-96 rounded-lg shadow-xs border ">
+    <div className="flex  items-center p-4">
+      <div className="p-3 rounded-full  dark:text-orange-100 bg-green-100 dark:bg-orange-500 mr-4">
+
+      </div>
+    <div>
+      <p class="mb-2 text-3xl font-medium text-gray-600 dark:text-gray-400">
+        CHANGE PERCENTAGE
+      </p>
+      <p class="text-5xl font-bold text-gray-700 dark:text-gray-200">
+        holder
+      </p>
+    </div>
+    </div>
+  </div>
 </div>
 
 </div>
--- frontend/src/components/Trade.js
+++ frontend/src/components/Trade.js
@@ -1,26 +1,100 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import {React, useState,useEffect } from "react";
 
+import { Link, useParams } from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
 
 
 function Trade() {
-    const location = useLocation();
+    const handlePurchase = (amount) => {
+        const purchaseData = {
+            id: uuidv4(),
+          ticker: data.box.ticker,
+          name: data.box.name,
+          purchasedate: new Date().toISOString(), // Set the purchase date
+          boughtfor: data.box.currentPrice,
+          ammountbought: amount, // Adjust as needed
+          total: data.box.currentPrice,
+        };
+    
+        // Retrieve existing purchases from Local Storage or initialize an empty array
+        const storedPurchases = JSON.parse(localStorage.getItem('purchases')) || [];
+    
+        // Add the new purchase to the array
+        storedPurchases.push(purchaseData);
+    
+        // Store the updated array in Local Storage
+        localStorage.setItem('purchases', JSON.stringify(storedPurchases));
+    
+        // You can also redirect the user to a confirmation page or perform other actions
+      };
+    let params = useParams();
+    const [data, setData] = useState(null)
+    useEffect(() => { 
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`http://127.0.0.1:8000/api/ticker/${params.ticker}/`);
+          const test = await response.json();
 
-    // Access the additional data or props
-    const additionalProps = location.state;
+          console.log(test)
+          console.log(test.box)
+          // Handle the data obtained from the API call here
+          setData(test);
+          // You can use the "data" variable within this scope or call another function to handle the data.
+        } catch (error) {
+          // Handle error if the API call fails
+          console.error('Error fetching data:', error);
+        }
+      };
+      fetchData();
+    }, []);
+
+    const [quantity, setQuantity] = useState(0);
+    const handleQuantityChange = (event) => {
+        setQuantity(event.target.value);
+      };
     return (
 
-<div className='innerlayer bg-white h-[calc(100vh-2rem)] w-11/12 m-auto border rounded-2xl flex flex-col justify-between'>
+    <div className='innerlayer bg-white h-[calc(100vh-2rem)] w-11/12 m-auto border rounded-2xl flex flex-col justify-between'>
+            {data?
+            (    <div className="flex items-center justify-center h-screen bg-gray-100">
+            <div className="bg-white p-6 rounded-lg shadow-md text-center">
+              <h1 className="text-8xl font-semibold mb-4">Purchase {data.box.ticker.toUpperCase()}</h1>
+              <h2 className="text-3xl mb-2 font-semibold text-gray-500">Current Price: <span className="text-green-500">${data.top.regularMarketPrice}</span> </h2>
+                    <div className="mb-4">
+                <label className="block text-lg mb-2" htmlFor="quantity">
+                    Number of Stocks to Purchase
+                </label>
+                <input
+                    id="quantity"
+                    type="number"
+                    value={quantity}
+                    onChange={handleQuantityChange}
+                    className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+                </div>
+                <div className="flex">
+                    <Link to = {'/stocks/' + data.box.ticker}>
+                    <div className=" text-3xl border h-32 w-72 flex items-center justify-center rounded-lg cursor-pointer transition duration-300 hover:shadow-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-14">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+                        </svg>
 
-        <div>
-            <h1>Purchase</h1>
-            
+                            </div>
+                    </Link>
+                    <Link to = {'/'}>
+                    <div className="border h-32 w-72 flex items-center justify-center rounded-lg cursor-pointer transition duration-300 hover:shadow-lg bg-green-200">
+                                PURCHASE
+                            </div>
+                    </Link>
+
+
+
+
+                </div>
+
+            </div>
+          </div>) : (<div></div>)}
         </div>
-
-
-
-
-    </div>
 
 
     )
